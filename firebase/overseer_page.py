00fc0e9901d9295dc05810a2e5dcb0325250141c
Grey_Ham <graham.brown137@gmail.com>
2015-03-14 11:24:36 -0700

    Not working javascript item.

--- firebase/overseer_page.py
+++ firebase/overseer_page.py
@@ -1,16 +1,29 @@
 import web
+import json
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://curo.firebaseio.com/')
-result = firebase.get('/Tasks', None)
+
+#rawJson = firebase.get('/', None)
+#alerts = firebase.get('/alerts', None)
+#areas = firebase.get('/areas', None)
+#tasklist = firebase.get('/task', None)
+
+
 
 urls = (
     '/', 'index'
 )
 
+render = web.template.render('templates/')
+
 class index:
+
+    def __inti__(self):
+        pass
+
     def GET(self):
-        return result
+        return render.index()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
