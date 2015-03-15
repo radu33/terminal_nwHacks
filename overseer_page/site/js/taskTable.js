1a6bdfa71ffca931d88de42aa4708a7ae5e06d87
Grey_Ham <graham.brown137@gmail.com>
2015-03-15 04:55:38 -0700

    Working taskTable.

--- overseer_page/site/js/taskTable.js
+++ overseer_page/site/js/taskTable.js
@@ -1,46 +1,62 @@
     var body = document.getElementsByTagName("body")[0];
     var myDataRef = new Firebase('https://curo.firebaseio.com/');
 
- //   console.out(httpGet('https://curo.furebaseio.com/areas.json'));
     var taskList = myDataRef.child('tasks');
 
     var jsontemp = jQuery.getJSON('https://curo.firebaseio.com/tasks/.json');
-//    console.log(jsontemp.responseText);
- //   var parsedJson = $.parseJSON(jsontemp);
-//    console.log(parsedJson.count);
-
-$.getJSON('https://curo.firebaseio.com/tasks/.json', function(data) {
-        var items = [];
-        $.each(data, function (key, val) {
-            alert(key + val.desc);
-            var row = document.createElement("tr");
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(key + ": " + val.desc);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        });
-
-        $( "</table>", {
-            "class" : "my-new-table",
-            html: items.join("")
-        }).appendTo("body");
-    });
-
 
     var theTaskTable = document.createElement("taskTable");
     var tblBody = document.createElement("tblBody");
 
-    for (iTask = 0; iTask < taskList.length; iTask++) {
-        var row = document.createElement("tr");
-        for (iField = 0; iField < taskList[iTask],length; iField++) {
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode(taskList[iTask][iField]);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        }
-        tblBody.appendChild(row);
-    }
+$.getJSON('https://curo.firebaseio.com/tasks/.json', function(data) {
+        var headerRow = document.createElement("tr");
+        var header1 = document.createElement("th")
+        header1.appendChild(document.createTextNode("Description"));
+        headerRow.appendChild(header1);
+        var header2 = document.createElement("th")
+        header2.appendChild(document.createTextNode("Location Num"));
+        headerRow.appendChild(header2);
+        var header3 = document.createElement("th")
+        header3.appendChild(document.createTextNode("Time Range"));
+        headerRow.appendChild(header3);
+        var header4 = document.createElement("th")
+        header4.appendChild(document.createTextNode("Progress"));
+        headerRow.appendChild(header4);
+        tblBody.appendChild(headerRow);
+        $.each(data, function (key, val) {
+            var row = document.createElement("tr");
+            var cell1 = document.createElement("td");
+            var cellDesc = document.createTextNode(val.desc + '\n');
+            cell1.appendChild(cellDesc);
+            row.appendChild(cell1);
+            var cell2 = document.createElement("td");
+            var cellLoc = document.createTextNode(val.location);
+            cell2.appendChild(cellLoc);
+            row.appendChild(cell2);
+            var cellTime = document.createTextNode(val.time.start + "-" + val.time.finish);
+            var cell3 = document.createElement("td");
+            cell3.appendChild(cellTime);
+            row.appendChild(cell3);
+            var completionString = ""
+            if (val.completion = 0) {
+                completionString = "Done";
+            }; 
+            if (val.completion = 1) {
+                completionString = "In Progress";
+            } else {
+                completionString = "Not Started";
+            }
+            var cellProg = document.createTextNode(completionString);
+            var cell4 = document.createElement("td");
+            cell4.appendChild(cellProg);
+            row.appendChild(cell4);
+            tblBody.appendChild(row);
+            
+        });
+    });
+
+
     theTaskTable.appendChild(tblBody);
     body.appendChild(theTaskTable);
-
-//window.alert("This is the alert");
+    theTaskTable.setAttribute("border", "1");
+    theTaskTable.setAttribute("padding", "3px");
